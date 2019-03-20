53ee103978ebd07bbbeacdab708f1c9baae7ded3
Tissiana <laratissiana@gmail.com>
2019-03-20 01:51:31 -0700

    finished

--- excercises-2.js
+++ excercises-2.js
@@ -4,6 +4,14 @@
 // if-then-else construct available in JavaScript.
 // ---------------------
 
+function max(num1, num2){
+    if (num1 > num2){
+        return num1;
+    }else{
+        return num2
+    }
+}
+
 console.assert(max(2,3) === 3)
 console.assert(max(23,24) === 24)
 console.assert(max(-23, 12) === 12)
@@ -12,6 +20,19 @@ console.assert(max(-23, 12) === 12)
 // Define a function maxOfThree() that takes three 
 // numbers as arguments and returns the largest of them.
 // ---------------------
+function maxOfThree(num1, num2, num3){
+    if(num1> num2 && num1 >num3){
+    return num1
+    }else if(num2 > num3 && num2 > num1){
+    return num2
+    }else{
+    return num3 
+    }
+}
+
+  var max = maxOfThree(2,56,3)
+  var max = maxOfThree(12,3,4)
+  var max = maxOfThree(-12,4,-5)
 
 console.assert(maxOfThree(2,56,3) === 56)
 console.assert(maxOfThree(12,3,4) === 12)
@@ -22,6 +43,10 @@ console.assert(maxOfThree(-12,4,-5) === 4)
 // string of length 1) and returns true if it is a 
 // vowel, false otherwise.
 // ---------------------
+
+
+//I DON'T KNOW HOW, NEED HELP PLEASE!
+
 
 console.assert(isVowel(0) === false);
 console.assert(isVowel("B") === false);
@@ -38,6 +63,8 @@ console.assert(isVowel("2") === false)
 // the string "tothohisos isos fofunon".
 // ---------------------
 
+//I DON'T KNOW HOW, NEED HELP PLEASE!
+
 console.assert(rovarspraket("a") === "a")
 console.assert(rovarspraket("b") === "bob")
 console.assert(rovarspraket("cat") === "cocatot")
@@ -50,7 +77,11 @@ console.assert(rovarspraket(0) === "0")
 // of a string. For example, reverse("jag testar") should 
 // return the string "ratset gaj".
 // ---------------------
-
+function reverse(str) {
+    return str;
+} 
+ var str = reverse("books")
+ var str = reverse("we don't want no trouble")
 
 console.assert(reverse("books") === "skoob")
 console.assert(reverse("we don't want no trouble") === "elbuort on tnaw t'nod ew")
